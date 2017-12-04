91fc9694e7f3b0b851993a5fdb3f854633628ce0
klaustaj <klaustaj@rose-hulman.edu>
2017-12-04 16:41:28 -0500

    done

--- src/m7_why_parameters_are_powerful.py
+++ src/m7_why_parameters_are_powerful.py
@@ -2,18 +2,18 @@
 This module lets you experience the POWER of FUNCTIONS and PARAMETERS.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Aaron Klaustermeier.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_draw_circles()
+    # run_test_draw_circles()
     # Un-comment the next lines when you are ready to use them.
     # run_test_better_draw_circles()
-    # run_test_even_better_draw_circles()
+    run_test_even_better_draw_circles()
 
 
 # ----------------------------------------------------------------------
@@ -64,7 +64,7 @@ def draw_circles():
     window.close_on_mouse_click()
 
 # ----------------------------------------------------------------------
-# TODO: 2.
+# DONE: 2.
 #   First, RUN this program.  You will see that draw_circles draws
 #   concentric circles whose radii vary by 10.
 #
@@ -92,10 +92,37 @@ def draw_circles():
 #   run_test_draw_circles  may get you started more quickly on your new
 #   better_draw_circles  and  run_test_better_draw_circles.
 # ----------------------------------------------------------------------
+def run_test_better_draw_circles():
+    print()
+    print('--------------------------------------------------')
+    print('Testing  better_draw_circles:  See graphics window')
+    print('--------------------------------------------------')
+
+    # Test 1:
+    delta_r = 3
+    print('Test 1 radius step size:', delta_r)
+    better_draw_circles(delta_r)
+
+    # Test 2:
+    delta_r = 7
+    print('Test 2 radius step size:', delta_r)
+    better_draw_circles(delta_r)
+
+
+def better_draw_circles(delta_r):
+    window = rg.RoseWindow(400, 400)
+
+    center = rg.Point(200, 200)
+    for k in range(21):
+        circle = rg.Circle(center, delta_r * k)
+        circle.attach_to(window)
+        window.render(0.05)  # Pauses for 0.05 seconds after rendering.
+
+    window.close_on_mouse_click()
 
 
 # ----------------------------------------------------------------------
-# TODO: 3.
+# DONE: 3.
 #   In the previous exercise, you made a MORE POWERFUL version
 #   of draw_circles by introducing a PARAMETER for the amount by
 #   which the radii of the concentric circles increase.
@@ -122,6 +149,43 @@ def draw_circles():
 #   In testing your even_better_draw_circles function,
 #   can you make some fun pictures?
 # ----------------------------------------------------------------------
+def run_test_even_better_draw_circles():
+    print()
+    print('-------------------------------------------------------')
+    print('Testing  even_better_draw_circles:  See graphics window')
+    print('-------------------------------------------------------')
+
+    # Test 1:
+    delta_r = 5
+    delta_center_x = 4
+    delta_center_y = 3
+    print('Test 1 radius step size:', delta_r)
+    print('Test 1 center x-coordinate step size:', delta_center_x)
+    print('Test 1 center y-coordinate step size:', delta_center_y)
+    even_better_draw_circles(delta_r, delta_center_x, delta_center_y)
+
+    # Test 2:
+    delta_r = 3
+    delta_center_x = -4
+    delta_center_y = -4
+    print('Test 2 radius step size:', delta_r)
+    print('Test 2 center x-coordinate step size:', delta_center_x)
+    print('Test 2 center y-coordinate step size:', delta_center_y)
+    even_better_draw_circles(delta_r, delta_center_x, delta_center_y)
+
+
+def even_better_draw_circles(delta_r, delta_x, delta_y):
+    window = rg.RoseWindow(400, 400)
+
+    center = rg.Point(200, 200)
+    for k in range(21):
+        center = rg.Point(200 + delta_x * k, 200 + delta_y * k)
+        circle = rg.Circle(center, delta_r * k)
+        circle.attach_to(window)
+        window.render(0.05)  # Pauses for 0.05 seconds after rendering.
+
+    window.close_on_mouse_click()
+
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
